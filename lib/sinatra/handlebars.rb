7aebcf3d94f26e98c310a84d1d50f36ec1fc1b88
Ben Kitzelman <benkitzelman@gmail.com>
2012-08-02 17:43:20 +1000

    Initial commit

---
+++ lib/sinatra/handlebars.rb
@@ -0,0 +1,83 @@
+module Sinatra
+  module Handlebars
+    def self.version
+      "0.0.1"
+    end
+
+    def self.registered(app)
+      app.extend ClassMethods
+    end
+
+    module ClassMethods
+      # set ember options
+      def handlebars(&block)
+        @handlebars_options ||= Options.new(self, &block)
+        self.handlebars_init! if block_given?
+        @handlebars_options
+      end
+
+      def handlebars_init!
+        handlebars.template_packages.each do |route, globs|
+          get route do
+            mtime, output = @template_cache.fetch(route) do
+              # find all the template files
+              paths = globs.map do |glob|
+                glob = File.expand_path(File.join(settings.root, glob))
+                Dir[glob].map { |x| x.squeeze('/') }
+              end
+              paths = paths.flatten.uniq
+
+              # build up template hash
+              template_paths = {}
+              paths.each do |path|
+                template_paths[File.basename(path, '.hbs')] = path
+              end
+
+              # build up the javascript
+              templates = template_paths.map do |(name, path)|
+                content = File.read(path)
+                "HandlebarsTemplates[#{name.inspect}] = Handlebars.compile(#{content.inspect});"
+              end
+
+              # wrap it up in a closure
+              output = %{
+                (function() {
+                  #{templates.join("\n")}
+                })();
+              }
+              output = output.strip.gsub(/^ {16}/, '')
+
+              # compute the maximum mtime for all paths
+              mtime = paths.map do |path|
+                if File.file?(path)
+                  File.mtime(path).to_i
+                end
+              end.compact.max
+
+              [mtime, output]
+            end
+
+            content_type :js
+            last_modified mtime
+            output
+          end
+        end
+      end
+    end
+
+    class Options
+      attr_reader :template_packages
+
+      def initialize(app, &block)
+        @app = app
+        @template_packages = {}
+
+        instance_eval(&block)
+      end
+
+      def templates(route, files=[])
+        @template_packages[route] = files
+      end
+    end
+  end
+end
---
+++ sinatra-handlebars.gemspec
@@ -0,0 +1,14 @@
+require './lib/sinatra/ember'
+Gem::Specification.new do |s|
+  s.name        = 'sinatra-handlebars'
+  s.version     = Sinatra::Handlebars.version
+  s.summary     = 'Helpers for serving a handlebars templates to an app from Sinatra.'
+  s.description = 'Serves Handlebars pages.'
+  s.authors     = ['Ben Kitzelman']
+  s.email       = ['benkitzelman@hotmail.com']
+  s.homepage    = 'http://github.com/benkitzelman/sinatra-handlebars'
+  s.files       = `git ls-files`.strip.split("\n")
+  s.executables = Dir['bin/*'].map { |f| File.basename(f) }
+
+  s.add_dependency 'sinatra'
+end
